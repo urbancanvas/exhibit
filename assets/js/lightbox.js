52ff7bd2f6b5d87f21f57ce9f642a8fbdc0720c5
urbancanvas <130040064+urbancanvas@users.noreply.github.com>
2023-04-06 15:52:46 +0800

    Add files via upload
    initial commit

---
+++ assets/js/lightbox.js
@@ -0,0 +1,2 @@
+function myFunction(){
+    $('#featured-works a, .lightBoxVideoLink').simpleLightbox();}
---
+++ assets/js/slider.js
@@ -0,0 +1,10 @@
+new Splide( '#committees',{
+    type   : 'loop',
+    lazyload: 'nearby',
+} ).mount();
+
+new Splide( '#about-slider',{
+    type   : 'loop',
+    arrows     : false,
+    
+} ).mount();
