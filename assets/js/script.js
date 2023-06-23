d577617f88821de4ed79ffca0ff1d68db71db544
urbancanvas <130040064+urbancanvas@users.noreply.github.com>
2023-06-23 19:17:57 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,3 +101,12 @@ function myFunction() {
   }
 }
 
+/* If browser back button was used, flush cache */
+(function () {
+	window.onpageshow = function(event) {
+		if (event.persisted) {
+			window.location.reload();
+		}
+	};
+})();
+
